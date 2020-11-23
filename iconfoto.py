12db550bc503ba5e17e382aaf0961901514d7778
BunyaminEfe <bnefe10@gmail.com>
2020-11-23 21:11:07 +0300

    guncelleme

--- iconfoto.py
+++ iconfoto.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Resource object code
-#
-# Created by: The Resource Compiler for PyQt5 (Qt v5.15.1)
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore
 
 qt_resource_data = b"\
